087717286ebd214547b6041245e73cb6661d8115
ldahmadkhaira <khairasyidiqi@gmail.com>
2023-07-20 11:59:43 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// toogle class active
+// maksudnya yang tadinya tidak ada menjadi ada dan sebaliknya
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar side bar untuk menghilagkan navbar mode table atau mobile
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
